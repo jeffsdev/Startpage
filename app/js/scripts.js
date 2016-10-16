2eaac6ed9fa356f92f0f4da3e957288c4f738603
Jeff Seymour <seym1585@gmail.com>
2016-10-15 22:26:43 -0700

    fix bug in bookmark js

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -34,4 +34,99 @@ $(document).ready(function() {
   update();
   setInterval(update, 1000);
 
+  // BOOKMARKS //////////////////////////////////////////////
+
+  var bookmarks = [
+    {
+      "category": "4chan",
+      "links": [
+        {
+          "url": "http://www.4chan.org/c/",
+          "name": "/c/ - Anime / Cute"
+        },
+        {
+          "url": "http://www.4chan.org/a/",
+          "name": "/a/ - Anime / Manga"
+        },
+        {
+          "url": "http://www.4chan.org/b/",
+          "name": "/b/ - Random"
+        },
+        {
+          "url": "http://www.4chan.org/g/",
+          "name": "/g/ - Technology"
+        },
+        {
+          "url": "http://www.4chan.org/tg/",
+          "name": "/tg/ - Traditional Games"
+        },
+        {
+          "url": "http://www.4chan.org/v/",
+          "name": "/v/ - Video Games"
+        },
+        {
+          "url": "http://www.4chan.org/vg/",
+          "name": "/vg/ - Video Games General"
+        },
+      ]
+    },
+    {
+      "category": "Social Media",
+      "links": [
+        {
+          "url": "http://www.facebook.com",
+          "name": "Facebook"
+        },
+        {
+          "url": "http://www.twitter.com",
+          "name": "Twitter"
+        },
+        {
+          "url": "http://www.linkedin.com",
+          "name": "LinkedIn"
+        },
+      ]
+    },
+    {
+      "category": "Coding",
+      "links": [
+        {
+          "url": "http://www.github.com",
+          "name": "GitHub"
+        },
+        {
+          "url": "http://www.stackoverflow.com",
+          "name": "Stack Overflow"
+        },
+      ]
+    },
+  ];
+
+
+
+  bookmarks.forEach(function(i) {
+    $(".bookmarks-container").append(
+      "<div class='bookmark-category'>" +
+      "<h3 class='bookmark-category-title'>" +
+      i.category +
+      "</h3><ul></ul></div>"
+    );
+    i.links.forEach(function(t) {
+      $(".bookmark-category:last-child ul").append(
+        "<li><a href='" +
+        t.url + "'>" +
+        t.name +
+        "</a></li>"
+      );
+    });
+  });
+  //
+  // $(".bookmarks-container").each(function() {
+  //
+  // })
+
+
+
+
+
 });
